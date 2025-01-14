22cf44ae5d33b336e20b6e6a0a0732588144fdc6
Mathieu Comandon <strycore@gmail.com>
2025-01-13 20:58:23 -0800

    Avoid getting lutris command on each loop iteration

--- core/library.gd
+++ core/library.gd
@@ -8,32 +8,32 @@ var lutris := Lutris.new()
 func get_library_launch_items() -> Array[LibraryLaunchItem]:
 	logger.info("Fetching Lutris library items")
 	var items: Array[LibraryLaunchItem] = []
-	
+
 	# Get the list of games from Lutris
 	var games := await lutris.get_games()
-	
+	# Get the lutris command to use
+	var lutris_cmd := await lutris.get_lutris_command()
+	var cmd := lutris_cmd.cmd
+	var args := lutris_cmd.args
 	# Create a launch item for each lutris game
 	for game in games:
 		if game.slug == "":
 			continue
-		
-		# Get the lutris command to use
-		var lutris_cmd := await lutris.get_lutris_command()
-		var cmd := lutris_cmd.cmd
-		var args := lutris_cmd.args
-		args.append("lutris:rungame/" + game.slug)
-		
+
+		var itemArgs = args.duplicate()
+		itemArgs.append("lutris:rungame/" + game.slug)
+
 		# Create a launch item for this game
 		var item := LibraryLaunchItem.new()
 		item.name = game.name
 		item.provider_app_id = game.slug
 		item.command = cmd
-		item.args = args
+		item.args = itemArgs
 		item.tags = ["lutris"]
 		item.installed = true
 		item.hidden = game.hidden
-		
+
 		items.append(item)
-	
+
 	logger.info("Found " + str(items.size()) + " games")
 	return items
